06bdcd312e57a6b9e1c74e957913409c7a1c3224
chuangmengtech <53854528+chuangmengtech@users.noreply.github.com>
2024-05-18 10:15:08 +0800

    Update main.go

--- main.go
+++ main.go
@@ -49,7 +49,7 @@ func SignIn(client *http.Client) bool {
 	params := u.Values{}
     	params.Set("sign", SIGN_KEY)
 	
-	reqest, err := http.NewRequest("POST", url, strings.NewReader(params.Encode()))
+	reqest, err := http.PostForm(url, strings.NewReader(params.Encode()))
 	reqest.Header.Add("Cookie", cookie)
 	reqest.Header.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36")
 	reqest.Header.Add("Content-Type", "multipart/form-data; boundary=<calculated when request is sent>")
